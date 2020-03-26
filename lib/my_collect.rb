5fcfca056e2af8e2c3f3cb5c96e597192bd59865
Christopher Reinken <christopherreinken@gmail.com>
2020-03-26 14:20:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,11 @@
 def my_collect(array)
   i = 0
   collection = []
-  while i < 0
+  while i < array.length
     collection << yield(array[i])
-    puts collection
     i += 1
   end
   collection
 end
 
-my_collect(["Tom", "Tim", "Tony"]) {|name| puts "Hi, #{name}."}
+# my_collect(["Tom", "Tim", "Tony"]) {|name| puts "Hi, #{name}."}
